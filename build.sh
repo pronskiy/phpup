61e7d92b121123a2c6e8af2a021e2f793f7690d7
Roman Pronskiy <roman@pronskiy.com>
2024-04-20 23:24:09 +0200

    Beta

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
-rm -rf ./build/*     
+rm -rf ./build/phpup     
+rm -rf ./build/index.*
+     
 box compile
 mkdir -p ./build
 mv index.phar ./build/
 
-#rm -rf ./tmp/*
-#cd tmp || exit
-#curl -O https://dl.static-php.dev/static-php-cli/common/php-8.3.3-micro-macos-aarch64.tar.gz
-#tar -xvf php-8.3.3-micro-macos-aarch64.tar.gz
-#cd ..
-
-cat ./tmp/micro.sfx ./build/index.phar > ./build/appbin && chmod 0755 ./build/appbin
+cd build || exit
+curl -O https://dl.static-php.dev/static-php-cli/common/php-8.3.4-micro-macos-aarch64.tar.gz -C -
+tar -xvf php-8.3.4-micro-macos-aarch64.tar.gz
+cat ./micro.sfx ./index.phar > ./phpup && chmod 0755 ./phpup
