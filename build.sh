82559e038922521cd3f3bc2d3784adb5d288bfcc
Roman Pronskiy <roman@pronskiy.com>
2024-05-05 19:09:44 +0200

    Update bundled PHP version

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ mkdir -p ./build
 mv index.phar ./build/
 
 cd build || exit
-curl -O https://dl.static-php.dev/static-php-cli/common/php-8.3.4-micro-macos-aarch64.tar.gz -C -
+curl -O https://dl.static-php.dev/static-php-cli/common/php-8.3.6-micro-macos-aarch64.tar.gz -C -
 tar -xvf php-8.3.4-micro-macos-aarch64.tar.gz
 cat ./micro.sfx ./index.phar > ./phpup && chmod 0755 ./phpup
