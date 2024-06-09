fffc15c2bc5c72c440708015deeabfe78b66c125
Roman Pronskiy <roman@pronskiy.com>
2024-06-09 20:32:03 +0200

    Update build setup

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: all build clean
+
+build:
+	./build.sh
--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 rm -rf ./build/phpup     
 rm -rf ./build/index.*
-     
+       
 box compile
 mkdir -p ./build
 mv index.phar ./build/
-
-cd build || exit
+  
+# shellcheck disable=SC2164
+cd build 
 curl -O https://dl.static-php.dev/static-php-cli/common/php-8.3.6-micro-macos-aarch64.tar.gz -C -
-tar -xvf php-8.3.4-micro-macos-aarch64.tar.gz
+tar -xvf php-8.3.6-micro-macos-aarch64.tar.gz
 cat ./micro.sfx ./index.phar > ./phpup && chmod 0755 ./phpup
