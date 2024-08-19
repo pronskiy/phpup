79fa349d1270cfada6bc2213e0eb5beb6bf919c0
Priyash <38959321+priyashpatil@users.noreply.github.com>
2024-08-19 13:43:16 +0530

    build: fix static php download

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
-rm -rf ./build/phpup     
+set -e
+
+rm -rf ./build/phpup
 rm -rf ./build/index.*
-       
+
 box compile
 mkdir -p ./build
 mv index.phar ./build/
-  
+
 # shellcheck disable=SC2164
-cd build 
-curl -O https://dl.static-php.dev/static-php-cli/common/php-8.3.6-micro-macos-aarch64.tar.gz -C -
+cd build
+curl -L -O https://dl.static-php.dev/static-php-cli/common/php-8.3.6-micro-macos-aarch64.tar.gz
 tar -xvf php-8.3.6-micro-macos-aarch64.tar.gz
-cat ./micro.sfx ./index.phar > ./phpup && chmod 0755 ./phpup
+cat ./micro.sfx ./index.phar >./phpup && chmod 0755 ./phpup
