1d57aecc6147f8b27bdfd6d873cf0f096ff9b523
Priyash <38959321+priyashpatil@users.noreply.github.com>
2024-08-19 13:54:05 +0530

    feat: add build os auto detection

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e
 
 rm -rf ./build/phpup
@@ -7,8 +8,33 @@ box compile
 mkdir -p ./build
 mv index.phar ./build/
 
-# shellcheck disable=SC2164
-cd build
-curl -L -O https://dl.static-php.dev/static-php-cli/common/php-8.3.6-micro-macos-aarch64.tar.gz
-tar -xvf php-8.3.6-micro-macos-aarch64.tar.gz
+# Detect OS and architecture
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    OS="macos"
+    if [[ $(uname -m) == "arm64" ]]; then
+        ARCH="aarch64"
+    else
+        ARCH="x86_64"
+    fi
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    OS="linux"
+    if [[ $(uname -m) == "aarch64" ]]; then
+        ARCH="aarch64"
+    else
+        ARCH="x86_64"
+    fi
+else
+    echo "Unsupported operating system"
+    exit 1
+fi
+
+# Construct the download URL
+PHP_VERSION="8.3.6"
+DOWNLOAD_URL="https://dl.static-php.dev/static-php-cli/common/php-${PHP_VERSION}-micro-${OS}-${ARCH}.tar.gz"
+
+# Download and extract
+cd build || exit
+echo "Downloading from: $DOWNLOAD_URL"
+curl -L -O "$DOWNLOAD_URL"
+tar -xvf "php-${PHP_VERSION}-micro-${OS}-${ARCH}.tar.gz"
 cat ./micro.sfx ./index.phar >./phpup && chmod 0755 ./phpup
